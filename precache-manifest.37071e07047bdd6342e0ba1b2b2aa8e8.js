1b2b020ba0b6727403b77418ab8c644b54c3ea54
Bach Vu <bachvu@1987>
2020-03-30 14:34:05 -0400

    Updates

---
+++ precache-manifest.37071e07047bdd6342e0ba1b2b2aa8e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c329bef1c09601f954476a7ef33d89",
+    "url": "/Robo-Friends/index.html"
+  },
+  {
+    "revision": "253952b190c22107d123",
+    "url": "/Robo-Friends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "45fea123676035cc4686",
+    "url": "/Robo-Friends/static/css/main.1a9dc46d.chunk.css"
+  },
+  {
+    "revision": "253952b190c22107d123",
+    "url": "/Robo-Friends/static/js/2.1660e621.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Robo-Friends/static/js/2.1660e621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45fea123676035cc4686",
+    "url": "/Robo-Friends/static/js/main.1c62f24c.chunk.js"
+  },
+  {
+    "revision": "394847eb68395ef9955c",
+    "url": "/Robo-Friends/static/js/runtime-main.1ec8704d.js"
+  }
+]);
